8a44431b8e643ae154dec9bbb0d5e27c46590064
ZaidKajee <zaidkajee2004@gmail.com>
2025-03-12 10:22:00 +0200

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,24 +1,19 @@
-#pragma once
-#include "Character.h"
 #ifndef ENEMY_H
 #define ENEMY_H
-#include <iostream>
-#include <string>
-#include <ctime>
-#include <cstdlib>
 
-class Enemy : public Character {
+#include <string>
+#include <iostream>
+
+class Enemy {
 private:
     int floor; // Floor level (1, 2, or 3)
-    int baseHealth; // Base health of the enemy
-    int StatReq[6]; // Stat requirements for defeating the enemy
+    int health; // Current health of the enemy
     bool isBoss; // Whether the enemy is a boss
 
 public:
-    Enemy(int floor, bool isBoss = false); // Constructor
+    Enemy(int floor); // Constructor
     bool isBossBattle() const; // Check if it's a boss battle
-    bool isDefeated(int Stats[6]); // Check if the enemy is defeated
     int getHealth() const; // Get the current health of the enemy
+    void takeDamage(int damage); // Reduce health by damage amount
+    bool isDefeated() const; // Check if the enemy is defeated
 };
-
-#endif
