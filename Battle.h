3edd3ec5bacbef44eabfb64e353ae721a55ed2d5
N4FS <nafsabdulla@gmail.com>
2025-04-08 11:56:04 +0200

    Update Battle.h

--- Battle.h
+++ Battle.h
@@ -6,16 +6,15 @@
 #include <string>
 #include "RoomBase.h"
 #include "Character.h"
-#include "Enemy.h" 
 #include "test.h"  
 
 
 class Battle : public RoomBase {
 private:
-   
+
 
 public:
-    Battle(bool isBoss);  // Constructor for Battle
+    Battle(bool isBoss,Character *player);  // Constructor for Battle
 
     int Calculate_Extra_Strikes(int agility);
     bool isCrit_Strike(int accuracy);
