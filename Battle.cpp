c8d34244f136d37974db3b5a65624d223498d208
N4FS <nafsabdulla@gmail.com>
2025-04-08 11:55:44 +0200

    Update Battle.cpp

--- Battle.cpp
+++ Battle.cpp
@@ -2,22 +2,22 @@
 #include "Battle.h"
 #include <random>
 #include <iostream>
+int extra;
 
-Battle::Battle(bool isBoss) : RoomBase("Battle") {
-    // Create the player and enemy objects
-    Character player("Player1");
+Battle::Battle(bool isBoss, Character* player) {
+    // Create the player and enemy object
 
-    test enemy(player.floor, isBoss);
-   
+    test enemy(player->floor, isBoss);
+    extra = Calculate_Extra_Strikes(player->statValue[3]);
 
     // Start the battle loop
     for (int i = 0; i < Total_Strikes(); i++) {
         while (!enemy.isDefeated()) {  // Check if the enemy is defeated
-            if (isCrit_Strike(player.statValue[5])) {
-                enemy.takeDamage(player.statValue[0] * 2);  // Apply double damage for crit strike
+            if (isCrit_Strike(player->statValue[5])) {
+                enemy.takeDamage(player->statValue[0] * 2);  // Apply double damage for crit strike
             }
             else {
-                enemy.takeDamage(player.statValue[0] * 1);  // Apply normal damage
+                enemy.takeDamage(player->statValue[0] * 1);  // Apply normal damage
             }
         }
     }
@@ -25,19 +25,22 @@ Battle::Battle(bool isBoss) : RoomBase("Battle") {
     // After battle, handle the outcome
     if (enemy.isDefeated()) {
         for (int i = 0; i < 6; i++) {
-            player.statValue[i] += 2;  // Gain +2 in all attributes for winning
+            player->statValue[i] += 2;  // Gain +2 in all attributes for winning
         }
 
         if (isBoss) {  // If the enemy is a boss, progress the player to the next floor
-            player.floor++;
+            player->floor++;
         }
     }
     else {
         for (int i = 0; i < 6; i++) {
-            player.statValue[i] -= 2;  // Lose -2 in all attributes for losing
+            player->statValue[i] -= 2;  // Lose -2 in all attributes for losing
         }
     }
 }
+
+
+
 
 int Battle::Calculate_Extra_Strikes(int agility) {
     return agility / 3;  // Calculate extra strikes based on agility
@@ -64,5 +67,5 @@ int Battle::Total_Strikes() {
     std::uniform_int_distribution<> dist(3, 5);
     int random_number = dist(gen);
 
-    return random_number + Calculate_Extra_Strikes(player.statValue[3]);  // Example player agility value
+    return random_number + extra;  // Example player agility value
 }
