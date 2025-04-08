08637906e714ca7f143b47ad7b5db415d34bbcaa
Zaheera Ganie <zaheeraganie@gmail.com>
2025-04-08 14:05:04 +0200

    Update Battle.h

--- Battle.h
+++ Battle.h
@@ -8,17 +8,30 @@
 #include "Character.h"
 #include "test.h"  
 
-
 class Battle : public RoomBase {
 private:
-
+    test* enemy;  // Declare a pointer to the test class (the enemy)
+    int strikesRemaining;  // Number of strikes remaining for the player
+    int extra;  // Extra strikes based on player agility
 
 public:
-    Battle(bool isBoss,Character *player);  // Constructor for Battle
+    //constructor
+    Battle(bool isBoss, Character* player);  // Constructor for Battle
+    //methods
+    void attack(Character* player);  // Method to handle player attack
+    int Calculate_Extra_Strikes(int agility);  // Calculate extra strikes based on agility
+    bool isCrit_Strike(int accuracy);  // Determine if it's a critical strike
+    int Total_Strikes();  // Total number of strikes (including extra strikes)
+    bool isBattleFinished();  // Check if the battle is finished (based on strikes or defeat)
+    // Getter for enemy (accessor method)
+    test* getEnemy() const {
+        return enemy;
+    }
 
-    int Calculate_Extra_Strikes(int agility);
-    bool isCrit_Strike(int accuracy);
-    int Total_Strikes();
+    //destructor
+    ~Battle() {
+        delete enemy;  // Clean up memory for enemy when Battle is done
+    }
 };
 
 #endif // BATTLE_H
